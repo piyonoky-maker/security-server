f14fae07ce9496d4fba1b21d59569e87c8e7c92f
가람 <piyonoky@gmail.com>
2026-01-30 10:38:29 +0900

    fix4

--- src/test/java/com/example/demo/HomeController.java
+++ src/test/java/com/example/demo/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Home Page 2-1";
+        return "Home Page 2-2";
     }
 }
